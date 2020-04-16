2a125d0b2a3b2010145535f7ad49875560e32f6f
Hanna Sarokina <tore.smile.01@gmail.com>
2020-04-16 14:14:38 +0300

    data types

---
+++ HelloWorld/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.lang.String;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        byte num = 23;
+        int num_1 = 462782;
+
+        float num_2 = 23.45f;
+        double num_3 = 3572.4893020484738d;
+
+        boolean isTrue = true; //1
+        char sym = 'R';
+
+        String str = "Hello World!";
+
+        System.out.println(str);
+    }
+}
