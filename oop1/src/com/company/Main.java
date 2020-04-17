3b2d803d3240d14002928ab0cb1b56efe53424e9
Hanna Sarokina <tore.smile.01@gmail.com>
2020-04-17 18:45:53 +0300

    Add constructors
    Add constructors to class Person

--- oop1/src/com/company/Main.java
+++ oop1/src/com/company/Main.java
@@ -3,12 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    Person vitya = new Person();
+	    Person vitya = new Person("Vitya");
 	    vitya.height = 120;
 	    vitya.say("George");
-        System.out.println(vitya.height);
+        System.out.println(vitya.name + "\'s height: " + vitya.height);
+
         Person vlad = new Person();
         System.out.println(vlad.height);
+
+        Person dima = new Person("Dima");
+        System.out.println(dima.name);
 
 
     }
--- oop1/src/com/company/Person.java
+++ oop1/src/com/company/Person.java
@@ -2,8 +2,19 @@ package com.company;
 
 public class Person {
     public int height = 180;
+    public String name = "Default";
 
     public void say (String name){
         System.out.println("Hello, " + name);
     }
+
+    public Person  (){
+
+    }
+
+    public Person  (String n){
+       name = n;
+    }
+
+
 }
